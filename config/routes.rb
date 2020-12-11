8be8df18797ff3c18304a3d2c185b7401a464857
Julio Hernandez <julioandres72@gmail.com>
2020-12-11 11:28:34 -0500

    almost

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   resources :posts
+  {:action=>"show", :controller=>"posts", :id=>nil}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
